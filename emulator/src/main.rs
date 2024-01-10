5135af4bb288fefc5362e6b3d2fceb742da422ad
__Vedant__ <vedantpahune@gmail.com>
2024-01-10 10:20:01 +0530

    Add ppu module

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 pub mod cpu;
+pub mod ppu;
 fn main() {
     println!("Hello, world!");
 }
---
+++ emulator/src/ppu/mod.rs
@@ -0,0 +1,13 @@
+pub struct PPU {}
+
+impl PPU {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl PPU {
+    pub fn step(&mut self) {
+        todo!("Implement PPU step")
+    }
+}
