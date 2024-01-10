ddd8ec94485fb995187c34349e962948f13f3d86
__Vedant__ <vedantpahune@gmail.com>
2024-01-10 11:13:46 +0530

    Add fields to PPU struct and add todos in step method

--- emulator/src/ppu/mod.rs
+++ emulator/src/ppu/mod.rs
@@ -1,13 +1,47 @@
-pub struct PPU {}
+pub struct PPU {
+    dot: u16,  // 0-340
+    line: u16, // 0-261, 0-239=visible, 240=post, 241-260=vblank, 261=pre
+
+    // PPU Registers
+    ppuctrl: u8,   // $2000
+    ppumask: u8,   // $2001
+    ppustatus: u8, // $2002
+    oamaddr: u8,   // $2003
+    oamdata: u8,   // $2004
+    ppuscroll: u8, // $2005
+    ppuaddr: u8,   // $2006
+    ppudata: u8,   // $2007
+
+    // Loopy Registers
+    v: u16,    // Current VRAM address (15 bits)
+    t: u16,    // Temporary VRAM address (15 bits)
+    x: u8,     // Fine X scroll (3 bits)
+    w: bool,   // First or second write toggle (1 bit)
+    
+    odd: bool, // odd frame flag
+    frame_counter: u64,
+}
 
 impl PPU {
     pub fn new() -> Self {
-        Self {}
+        todo!("Implement PPU::new")
     }
 }
 
 impl PPU {
     pub fn step(&mut self) {
-        todo!("Implement PPU step")
+        // todo: implement rendering
+
+        // todo: implement background data fetching
+
+        // todo: implement sprite data fetching
+
+        // todo: implement vblank entering
+
+        // todo: implement vblank exiting
+        
+        // todo: implement nmi handling
+                              
+        // todo: implement increment dot, line and frame counters
     }
 }
