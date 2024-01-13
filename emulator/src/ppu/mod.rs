b55cf135feda90e7d0b36260c7f65aee1dc07b98
__Vedant__ <vedantpahune@gmail.com>
2024-01-13 17:20:40 +0530

    Implement render function

--- emulator/src/ppu/mod.rs
+++ emulator/src/ppu/mod.rs
@@ -17,7 +17,7 @@ pub struct PPU {
     t: u16,  // Temporary VRAM address (15 bits)
     x: u8,   // Fine X scroll (3 bits)
     w: bool, // First or second write toggle (1 bit)
-    
+
     /*
         pixel color is 4 bits:
             ( px = 4 bits )
@@ -61,7 +61,7 @@ impl PPU {
         let render_time = visible_line && visible_dot;
         let fetch_time = fetch_line && fetch_dot;
 
-        let rendering_enabled = true; // todo!("Implement rendering_enabled");
+        let rendering_enabled = self.is_rendering_enabled();
 
         if rendering_enabled {
             ///// render //////
@@ -181,10 +181,66 @@ impl PPU {
         let y = self.line as usize;
 
         ///// handle clipping //////
+        let mut render_sp = self.bg_rendering_allowed();
+        let mut render_bg = self.sp_rendering_allowed();
+        if x < 8 {
+            if !self.leftmost_bg_rendering_allowed() {
+                render_bg = false;
+            }
+            if !self.leftmost_sp_rendering_allowed() {
+                render_sp = false;
+            }
+        }
 
-        ///// get final color//////
+        ///// get final color //////
+        let bg_color = if render_bg { self.get_bg_color() } else { None };
+        let sp_color = if render_sp { self.get_sp_color() } else { None };
+
+        let combined_color = match (bg_color, sp_color) {
+            (Some(bg), Some(sp)) => {
+                todo!("Implement sprite/background priority");
+            }
+            (Some(bg), None) => bg,
+            (None, Some(sp)) => sp,
+            (None, None) => (0, 0, 0),
+        };
 
         ///// put final color in frame buffer //////
+        if x < 256 && y < 240 {
+            let offset = (y * 256 + x) * 4;
+            self.frame_buffer[offset] = combined_color.0;
+            self.frame_buffer[offset + 1] = combined_color.1;
+            self.frame_buffer[offset + 2] = combined_color.2;
+            self.frame_buffer[offset + 3] = 255;
+        }
+    }
+
+    fn get_bg_color(&self) -> Option<(u8, u8, u8)> {
+        todo!();
+    }
+
+    fn get_sp_color(&self) -> Option<(u8, u8, u8)> {
+        todo!();
+    }
+
+    fn sp_rendering_allowed(&self) -> bool {
+        todo!();
+    }
+
+    fn bg_rendering_allowed(&self) -> bool {
+        todo!();
+    }
+
+    fn leftmost_sp_rendering_allowed(&self) -> bool {
+        todo!();
+    }
+
+    fn leftmost_bg_rendering_allowed(&self) -> bool {
+        todo!();
+    }
+
+    fn is_rendering_enabled(&self) -> bool {
+        self.bg_rendering_allowed() && self.sp_rendering_allowed()
     }
 
     //// fetch background ///////////////////////
