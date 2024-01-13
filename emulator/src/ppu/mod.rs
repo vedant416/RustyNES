9dc88bcbddefa54c40ae0e6d876bb672300f1566
__Vedant__ <vedantpahune@gmail.com>
2024-01-13 14:47:50 +0530

    Add shift register and implement fetch_bg

--- emulator/src/ppu/mod.rs
+++ emulator/src/ppu/mod.rs
@@ -17,6 +17,19 @@ pub struct PPU {
     t: u16,  // Temporary VRAM address (15 bits)
     x: u8,   // Fine X scroll (3 bits)
     w: bool, // First or second write toggle (1 bit)
+    
+    /*
+        pixel color is 4 bits:
+            ( px = 4 bits )
+
+        tile is 8x8 px (single tile row is 8px)
+            ( row_size = 8*4 = 32 bits )
+
+        shift_register stores pixels for 2 tile rows
+            ( 32 bits * 2 = 64 bits )
+            shift_register size = 64 bits
+    */
+    shift_register: u64,
 
     // memory
     frame_palette: [u8; 32],
@@ -176,7 +189,44 @@ impl PPU {
 
     //// fetch background ///////////////////////
     pub fn fetch_bg(&mut self) {
-        todo!("Implement PPU::fetch_bg")
+        // before fetching:
+        // shift the shift register by size of one pixel (4 bits)
+        // to make room for next pixel value (4 bits)
+        self.shift_register <<= 4;
+
+        // fetching:
+        match self.dot & 7 {
+            // fetchs tile id
+            1 => self.fetch_nt(),
+
+            // at is used to determine which palette is used
+            // (upper 2 bits of pixel value)
+            3 => self.fetch_at(),
+
+            // uses tile_id (fetched in fetch_nt) to fetch 2 bit-planes
+            // combine 2 bit-planes (each bit plane is 1 bit)
+            // (lower 2 bits of pixel value)
+            7 => self.fetch_pt(),
+
+            // after fetching:
+            // combine fetched data into pixel value
+            // upper 2 bits + lower 2 bits = 4bit pixel value
+            // load combined value (4 bits) into shift register
+            0 => self.load_fetched_data(),
+            _ => {}
+        }
+    }
+    fn fetch_nt(&mut self) {
+        todo!();
+    }
+    fn fetch_at(&mut self) {
+        todo!();
+    }
+    fn fetch_pt(&mut self) {
+        todo!();
+    }
+    fn load_fetched_data(&mut self) {
+        todo!();
     }
 
     //// fetch_sprites ///////////////////////
