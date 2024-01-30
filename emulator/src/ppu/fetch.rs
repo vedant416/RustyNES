95722238bd387578953137c8684b2a5a1a7bedd8
__Vedant__ <vedantpahune@gmail.com>
2024-01-30 15:48:42 +0530

    Implement PPU fetch functions

--- emulator/src/ppu/fetch.rs
+++ emulator/src/ppu/fetch.rs
@@ -30,15 +30,25 @@ impl super::PPU {
     }
 
     fn fetch_nt(&mut self) {
-        todo!();
+        let nt_addr = 0x2000 | (self.v & 0x0FFF);
+        self.nametable_latch = self.read_nametable(nt_addr);
     }
     
     fn fetch_at(&mut self) {
-        todo!();
+        let v = self.v;
+        let attr_addr = 0x23C0 | (v & 0x0C00) | ((v >> 4) & 0x38) | ((v >> 2) & 0b111);
+        let shift = ((v >> 4) & 4) | (v & 2);
+        self.attribute_table_latch = (self.read_nametable(attr_addr) >> shift) & 0b11;
     }
     
     fn fetch_pt(&mut self) {
-        todo!();
+        let table = self.background_pt_addr();
+        let tile_num = self.nametable_latch as u16;
+        let fine_y = ((self.v >> 12) & 0b111) as u8;
+        let addr = table + tile_num * 16 + fine_y as u16;
+
+        self.pattern_table_low_latch = self.read_chr(addr);
+        self.pattern_table_high_latch = self.read_chr(addr + 8);
     }
     
     fn load_fetched_data(&mut self) {
