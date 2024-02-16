42b23e73305b4408e02df981fcd79b51e6069cbe
__Vedant__ <vedantpahune@gmail.com>
2024-02-16 23:57:46 +0530

    Implement rom file parsing

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -1,11 +1,22 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
-pub mod cpu;
-pub mod ppu;
-pub mod rom;
-pub mod mappers;
+
+use crate::rom::create_cartridge;
+use std::{env::args, fs::read};
+
 pub mod bus;
 pub mod controller;
+pub mod cpu;
+pub mod mappers;
+pub mod ppu;
+pub mod rom;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    if args.len() != 2 {
+        panic!("Usage: emulator <path-to-rom-file>");
+    }
+    let rom_path = &args[1];
+    let bytes = read(rom_path).expect("Failed to read ROM file");
+    let cartridge = create_cartridge(bytes);
 }
--- emulator/src/rom.rs
+++ emulator/src/rom.rs
@@ -1,4 +1,4 @@
-use crate::mappers::Mapper;
+use crate::mappers::{nrom::NROM, Mapper};
 
 // mapper is a chip on the cartridge that controls
 // how the program code and graphics data are read from the PRG ROM and CHR ROM
@@ -35,6 +35,84 @@ pub struct ROM {
     // trainer is a 512-byte data, it is not required for emulation of NES
     // trainer flag is used to determine if the trainer is present in the rom file
     pub trainer: bool,
+}
+
+/*
+ines file format
+header (16 bytes)
+trainer (0 or 512 bytes)
+prg rom (16KB * number of prg rom banks)
+chr rom (8KB * number of chr rom banks)
+*/
+pub fn create_cartridge(bytes: Vec<u8>) -> Cartridge {
+    // Check file signature
+    let signature = &bytes[0..4];
+    if signature != [78, 69, 83, 26] {
+        panic!("Invalid file signature: {:?}", signature);
+    }
+
+    // Check ines version
+    let ines_version = (bytes[7] & 0b1100_0000) >> 2;
+    if ines_version != 0 {
+        panic!("Invalid ines version: {}", ines_version);
+    }
+
+    // number of PRG ROM and CHR ROM banks
+    let prg_rom_banks = bytes[4];
+    let chr_rom_banks = bytes[5];
+
+    // Check if trainer is present
+    let trainer = (bytes[6] & 0b0000_0100) != 0;
+
+    // Skip header bytes (16 bytes) and trainer bytes(0 or 512 bytes)
+    // PRG ROM starts after the header and trainer
+    let prg_rom_start = 16 + if trainer { 512 } else { 0 };
+    let prg_rom_end = prg_rom_start + (prg_rom_banks as usize) * 0x4000;
+
+    // CHR ROM starts after the PRG ROM
+    let chr_rom_start = prg_rom_end;
+
+    // Mirroring mode
+    let mirroring = if (bytes[6] & 0b0000_1000) != 0 {
+        Mirroring::FourScreen
+    } else if (bytes[6] & 0b1) != 0 {
+        Mirroring::Vertical
+    } else {
+        Mirroring::Horizontal
+    };
+
+    // Construct the mapper id from the header
+    let mapper_id = (bytes[7] & 0b1111_0000) | (bytes[6] >> 4);
+
+    println!("byte 0 {}", &bytes[0]);
+    println!("byte 1 {}", &bytes[1]);
+    println!("byte 2 {}", &bytes[2]);
+    println!("byte 3 {}", &bytes[3]);
+    println!("prg_rom_banks {}", &prg_rom_banks);
+    println!("chr_rom_banks {}", &chr_rom_banks);
+    println!("prg_rom_start {}", &prg_rom_start);
+    println!("chr_rom_start {}", &chr_rom_start);
+    println!("mirroring {:?}", &mirroring);
+    println!("mapper_id {}", &mapper_id);
+    println!("trainer {}", &trainer);
+
+    // Create ROM
+    let rom = ROM {
+        bytes,
+        prg_rom_banks,
+        chr_rom_banks,
+        prg_rom_start,
+        chr_rom_start,
+        mapper_id,
+        mirroring,
+        trainer,
+    };
+
+    let cartridge: Cartridge = match mapper_id {
+        0 => Box::new(NROM::new(rom)),
+        _ => panic!("Mapper not implemented: {}", rom.mapper_id),
+    };
+    cartridge
 }
 
 #[derive(Debug)]
