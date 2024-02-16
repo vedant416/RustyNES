69afe8135bcb498d26995dd5a47ac9aa0bc7d9a1
__Vedant__ <vedantpahune@gmail.com>
2024-02-16 17:21:14 +0530

    Implement Mapper trait for NROM

--- emulator/src/mappers/nrom.rs
+++ emulator/src/mappers/nrom.rs
@@ -1,16 +1,59 @@
+use crate::rom::ROM;
+use super::Mapper;
 
-pub struct NROM {}
+pub struct NROM {
+    prg_ram: [u8; 0x800],
+    rom: ROM,
+}
 
-impl super::Mapper for NROM {
-    fn get_data(&self) -> &crate::rom::ROM {
-        todo!()
+impl NROM {
+    pub fn new(rom: ROM) -> Self {
+        Self {
+            prg_ram: [0; 0x800],
+            rom,
+        }
     }
+}
 
+impl Mapper for NROM {
     fn read(&mut self, addr: u16) -> u8 {
-        todo!()
+        let addr = addr as usize;
+        match addr {
+            // CHR ROM
+            0x0000..=0x1FFF => {
+                let offset = self.rom.chr_rom_start + addr;
+                self.rom.bytes[offset]
+            }
+
+            // PRG RAM
+            0x6000..=0x7FFF => self.prg_ram[(addr - 0x6000) & 0x7FF],
+
+            // PRG ROM
+            0x8000..=0xFFFF => {
+                let offset = {
+                    let mut addr = addr - 0x8000;
+                    if self.rom.prg_rom_banks == 1 {
+                        addr &= 0x3FFF;
+                    }
+                    self.rom.prg_rom_start + addr
+                };
+                self.rom.bytes[offset]
+            }
+
+            _ => unreachable!("Invalid read address for NROM: {:#X}", addr),
+        }
     }
 
     fn write(&mut self, addr: u16, val: u8) {
-        todo!()
+        let addr = addr as usize;
+        match addr {
+            // PRG RAM
+            0x6000..=0x7FFF => self.prg_ram[(addr - 0x6000) & 0x7FF] = val,
+            _ => (),
+        }
+    }
+
+    fn get_data(&self) -> &ROM {
+        &self.rom
     }
 }
