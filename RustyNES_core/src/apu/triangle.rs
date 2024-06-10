83d0cde5326e0408011e29252f11e0e84fb7a575
__Vedant__ <vedantpahune@gmail.com>
2024-06-10 22:04:21 +0530

    Add fields to triangle struct

--- RustyNES_core/src/apu/triangle.rs
+++ RustyNES_core/src/apu/triangle.rs
@@ -1,6 +1,21 @@
+use super::units::{LengthCounter, Timer};
+
+const TRIANGLE: [u8; 32] = [
+    15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+    13, 14, 15,
+];
+
 #[derive(Default)]
 pub struct Triangle {
     enabled: bool,
+    duty_cycle: u8,
+    timer: Timer,
+    length_counter: LengthCounter,
+    // linear counter
+    control_flag: bool,
+    reload_flag: bool,
+    counter: u8,
+    reload_value: u8,
 }
 
 // Step /////
